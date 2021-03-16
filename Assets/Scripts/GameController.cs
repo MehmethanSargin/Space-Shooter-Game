dc0dcd84075bf49a0cc3dea959f51aef571a99be
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-17 00:21:07 +0300

    game controller added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject hazard;
+    [SerializeField]
+    int spawnCount;
+    [SerializeField]
+    float spawnWait;
+    [SerializeField] 
+    float startWait;
+    [SerializeField]
+    float waveWait;
+
+    void Start()
+    {
+        StartCoroutine(SpawnValues());
+    }
+
+    IEnumerator SpawnValues()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for(int i = 0; i<spawnCount; i++) 
+            { 
+                Vector3 spawnPosition = new Vector3(Random.Range(-2.9f,2.9f),0,9);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
