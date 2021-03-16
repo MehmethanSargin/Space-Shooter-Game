9054db764eea9eafd80f499bd811944cb1edfde1
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-16 22:12:56 +0300

    rotate asteroids

---
+++ Assets/Scripts/AsteroidsRotater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsRotater : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    private int rotateSpeed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.angularVelocity = Random.insideUnitSphere * rotateSpeed;
+    }
+}
