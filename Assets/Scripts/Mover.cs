f31d9952fa637b19d5b063169a66fb4e0ee11290
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-16 18:01:33 +0300

    bullet move

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] int speed;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+}
