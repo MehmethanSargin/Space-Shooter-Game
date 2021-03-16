6fa828a66cf25734a9f9b10c95b2f2ab9e8708b0
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-17 00:20:46 +0300

    destroy operations added

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    [SerializeField]
+    GameObject explosion;
+    [SerializeField]
+    GameObject playerExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Boundary") 
         {
             return;
         }
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    private float lifeTime = 2f;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
